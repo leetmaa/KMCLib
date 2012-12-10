5c257996631a104aa8e2d51e55f8f82009437b76
Mikael Leetmaa <mikael.leetmaa@gmail.com>
2012-12-10 10:26:58 +0100

    Task #0021: Implementing matching tests.

--- c++/unittest/test_matcher.cpp
+++ c++/unittest/test_matcher.cpp
@@ -9,8 +9,11 @@
 // Include the test definition.
 #include "test_matcher.h"
 
-// Include the files to test.
+// Include the file to test.
 #include "../src/matcher.h"
+
+#include "../src/matchlistentry.h"
+
 
 // -------------------------------------------------------------------------- //
 //
@@ -28,8 +31,38 @@ void Test_Matcher::testIsMatchMatchList()
     Matcher m;
 
     // Setup two match lists to check.
+    std::vector<MatchListEntry> process_match_list;
+    process_match_list.push_back(MatchListEntry(3,2,1.2,Coordinate(0.1,0.2,0.34)));
+    process_match_list.push_back(MatchListEntry(1,4,1.6,Coordinate(1.1,0.2,0.34)));
+    process_match_list.push_back(MatchListEntry(8,2,1.9,Coordinate(0.1,5.2,0.34)));
 
-    // NEEDS IMPLEMENTATION
+    std::vector<MatchListEntry> index_match_list;
+    index_match_list.push_back(MatchListEntry(3,2,1.2,Coordinate(0.1,0.2,0.34)));
+    index_match_list.push_back(MatchListEntry(1,4,1.6,Coordinate(1.1,0.2,0.34)));
+    index_match_list.push_back(MatchListEntry(8,2,1.9,Coordinate(0.1,5.2,0.34)));
+
+    // These two are equal.
+    CPPUNIT_ASSERT( m.isMatch(process_match_list, index_match_list) );
+
+    // It works also if the index match list is longer.
+    index_match_list.push_back(MatchListEntry(8,2,4.9,Coordinate(3.1,5.2,0.34)));
+
+    // These two are still equal.
+    CPPUNIT_ASSERT( m.isMatch(process_match_list, index_match_list) );
+
+    // If we add another not matching element to the process_match_list vecctor
+    // we get a non-match.
+    process_match_list.push_back(MatchListEntry(1,2,4.9,Coordinate(3.1,5.2,0.34)));
+
+    // These two are no longer equal.
+    CPPUNIT_ASSERT( !m.isMatch(process_match_list, index_match_list) );
+
+    // But if they differ in the update index they are still equal.
+    process_match_list[3] = MatchListEntry(8,1,4.9,Coordinate(3.1,5.2,0.34));
+
+    // These two are again equal.
+    CPPUNIT_ASSERT( m.isMatch(process_match_list, index_match_list) );
+
 }
 
 
