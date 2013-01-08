b366b95c0edc144043d20b929d9f6c22a48b1bf1
Mikael Leetmaa <mikael.leetmaa@gmail.com>
2013-01-08 15:32:17 +0100

    Task #0030: Ttrajectory implementation and tests.

--- python/src/KMCLib/Utilities/Trajectory/Trajectory.py
+++ python/src/KMCLib/Utilities/Trajectory/Trajectory.py
@@ -19,30 +19,70 @@ class Trajectory(object):
 
     def __init__(self,
                  trajectory_filename,
-                 sites):
+                 sites,
+                 max_buffer_size=None,
+                 max_buffer_time=None):
         """
         Constructor for the Trajectory.
 
         :param trajectory_filename: The file name to write trajectory information to.
+        :type trajectory_filename: str
 
-        :param sites: The lattice sites of the configuration.
+        :param sites: The lattice sites of the configuration as an Nx3 list.
+
+        :param max_buffer_size: The max size of the the buffer in memory
+                                before writing to file.
+        :type max_buffer_size: int
+
+        :param max_buffer_time: The max time limit between dumps to file.
+        :type max_buffer_time: float
         """
+        # Set the defaults.
+        if max_buffer_size is None:
+            self.__max_buffer_size = 1024*1024*10    #   <- Max buffer size in bytes (10 MB)
+        else:
+            self.__max_buffer_size = max_buffer_size
+
+        if max_buffer_time is None:
+            self.__max_buffer_time = 30*60.0           #   <- Max time in seconds (30 min)
+        else:
+            self.__max_buffer_time = max_buffer_time
+
         # Note that this object is not in the interface, so input is allready checked.
+        self.__trajectory_filename = trajectory_filename
 
         # Open the file and write the meta information.
-        # NEEDS IMPLEMENTATION
+        with open(self.__trajectory_filename, 'w') as trajectory:
+            trajectory.write( "# KMCLib Trajectory\n" )
+            trajectory.write( "version=\"2013.1.0\"\n" )
+            trajectory.write( "creation_time=\"%s\"\n"%(time.ctime()) )
 
-        # Write the sites.
-        # NEEDS IMPLEMENTATION
+            # Write the sites.
+            sites_str = "sites=["
+            indent    = " "*7
+            for i,site in enumerate(sites):
+                sites_str += "[%15.6f,%15.6f,%15.6f]"%(site[0],site[1],site[2])
+
+                # Handle the last site differently.
+                if i == len(sites)-1:
+                    sites_str += "]\n"
+                else:
+                    sites_str += ",\n" + indent
+
+            trajectory.write( sites_str )
+
+            # Write the empty lists.
+            trajectory.write("times=[]\n")
+            trajectory.write("steps=[]\n")
+            trajectory.write("types=[]\n")
 
         # Init the member data.
         self.__types_buffer = []
         self.__simulation_time_buffer = []
         self.__step_buffer = []
 
-        # The constants defining the buffering behaviour.
-        self.__max_buffer_size = 1024*1024*10    #   <- Max buffer size in bytes (10 MB)
-        self.__max_buffer_time = 30*60           #   <- Max time in seconds (30 min)
+        # Set the time counter to zero.
+        self.__time_last_dump = 0.0
 
     def append(self, simulation_time, step, types):
         """
@@ -66,27 +106,49 @@ class Trajectory(object):
         types_size = sys.getsizeof(self.__types_buffer)
 
         # Write if the buffer is to large of if enough time has passed.
-        if (types_size > self.__max_buffer_size):
-            self.__writeToFile(simulation_time, step, types)
+        if (types_size > self.__max_buffer_size) or \
+                ((time.time() - self.__time_last_dump) > self.__max_buffer_time):
+            # Wite.
+            self.__writeToFile(self.__simulation_time_buffer,
+                               self.__step_buffer,
+                               self.__types_buffer)
+            # Reset.
+            self.__simulation_time_buffer = []
+            self.__step_buffer            = []
+            self.__types_buffer           = []
 
-        elif ((time.time() - self.__time_last_dump) > max_buffer_time):
-            self.__writeToFile(simulation_time, step, types)
-
-    def __writeToFile(self, simulation_time, step, types):
+    def __writeToFile(self, simulation_time_buffer, step_buffer, types_buffer):
         """
         Append the types and time information to the trajectory.
         The trajectory if flushed to file if the flush time limit has passed.
 
-        :param simulation_time: The current time of the simulation.
-        :type simulation_time: float
-
-        :param step: The step number in the simulation.
-        :type step: int
-
-        :param types: The types given as a list of strings.
+        :param simulation_time_buffer: A list of simulation times to be written to disk.
+        :param step_buffer:            A list of step numbers to be written.
+        :param types_buffer:           The types buffer given as a list of lists of strings.
         """
         # Save to file.
-        # NEEDS IMPLEMENTATION
+        with open(self.__trajectory_filename, 'a') as trajectory:
+            for (sim_time, step, types) in zip(simulation_time_buffer, step_buffer, types_buffer):
+                trajectory.write( "times.append(%f)\n"%sim_time )
+                trajectory.write( "steps.append(%i)\n"%step )
+
+                # Write the types.
+                types_str = "types.append(["
+                indent = " "*14
+                row_length = len(types_str)
+                for t in types[:-1]:
+                    row_length += len(t) + 2
+                    types_str += "\"" + t + "\"" + ","
+
+                    # Berak the row if above 70 positions.
+                    if row_length >= 70:
+                        types_str += "\n" + indent
+                        row_length = len(indent)
+                # Add the last type.
+                types_str += "\"" + types[-1] + "\"" + "])\n"
+
+                # Write it to file.
+                trajectory.write(types_str)
 
         # Update the time.
         self.__time_last_dump = time.time()
--- python/unittest/KMCLibTest/Utilities/Trajectory/TrajectoryTest.py
+++ python/unittest/KMCLibTest/Utilities/Trajectory/TrajectoryTest.py
@@ -8,19 +8,342 @@
 #
 
 import unittest
+import os
+import numpy
+import time
+import sys
 
 # Import from the module we test.
-from KMCLib.Utilities.Trajectory import Trajectory
+from KMCLib.Utilities.Trajectory.Trajectory import Trajectory
 
 
 # Implement the test.
 class TrajectoryTest(unittest.TestCase):
     """ Class for testing the Trajectory object. """
 
+    def setUp(self):
+        """ The setUp method for test fixtures. """
+        self.__files_to_remove = []
+
+    def tearDown(self):
+        """ The tearDown method for test fixtures. """
+#        for f in self.__files_to_remove:
+#            os.remove(f)
+
     def testConstruction(self):
         """ Test the Trajectory object can be constructed. """
-        # NEEDS IMPLEMENTATION
-        pass
+        # Setup input.
+        sites = [[1.0,1.0,2.3],
+                 [3.4,4.5,4.3],
+                 [3.7,7.5,6.5]]
+        name = os.path.abspath(os.path.dirname(__file__))
+        name = os.path.join(name, "..", "..")
+        name = os.path.join(name, "TestUtilities", "Scratch")
+        trajectory_filename = os.path.join(name, "tmp_trajectory_file.py")
+        self.__files_to_remove.append(trajectory_filename)
+
+        # Construct with default values.
+        dt = Trajectory(trajectory_filename, sites)
+
+        # Check the defaults.
+        self.assertAlmostEqual( dt._Trajectory__max_buffer_time,  60*30 )
+        self.assertEqual( dt._Trajectory__max_buffer_size, 1024*1024*10 )
+
+        # Construct.
+        t = Trajectory(trajectory_filename,
+                       sites,
+                       max_buffer_time=100.0,
+                       max_buffer_size=100000)
+
+        # Check the stored values.
+        self.assertEqual( t._Trajectory__max_buffer_time,  100.0 )
+        self.assertEqual( t._Trajectory__max_buffer_size, 100000 )
+        self.assertEqual( t._Trajectory__trajectory_filename, trajectory_filename )
+
+        # Open the trajectory file and check that we can read the meta
+        # information and sites.
+        global_dict = {}
+        local_dict  = {}
+        execfile(trajectory_filename, global_dict, local_dict)
+
+        # Get the version and creation time.
+        read_version  = local_dict["version"]
+        read_creation = local_dict["creation_time"]
+
+        # Check that they are of corect type.
+        self.assertTrue( isinstance(read_version, str) )
+        self.assertTrue( isinstance(read_creation, str) )
+
+        # Check the sites.
+        read_sites = numpy.array(local_dict["sites"])
+        ref_sites  = numpy.array(sites)
+        self.assertAlmostEqual( numpy.linalg.norm(read_sites - ref_sites), 0.0, 10 )
+
+        # Check the empty lists.
+        read_times = local_dict["times"]
+        read_steps = local_dict["steps"]
+        read_types = local_dict["types"]
+        empty_list = []
+        self.assertEqual( read_times, empty_list )
+        self.assertEqual( read_steps, empty_list )
+        self.assertEqual( read_types, empty_list )
+
+    def testVersion(self):
+        """ Test the Trajectory file version number string. """
+        # Setup input.
+        sites = [[1.0,1.0,2.3],
+                 [3.4,4.5,4.3],
+                 [3.7,7.5,6.5]]
+        name = os.path.abspath(os.path.dirname(__file__))
+        name = os.path.join(name, "..", "..")
+        name = os.path.join(name, "TestUtilities", "Scratch")
+        trajectory_filename = os.path.join(name, "tmp_trajectory_file.py")
+        self.__files_to_remove.append(trajectory_filename)
+
+        # Construct.
+        t = Trajectory(trajectory_filename, sites)
+
+        # Open the trajectory file and check that we can read the meta
+        # information and sites.
+        global_dict = {}
+        local_dict  = {}
+        execfile(trajectory_filename, global_dict, local_dict)
+
+        # Get the version and check.
+        read_version  = local_dict["version"]
+        ref_version = "2013.1.0"
+        self.assertEqual( read_version, ref_version )
+
+    def testWriteToFile(self):
+        """ Test writing the buffers to file. """
+        # Setup input.
+        sites = [[0.0,1.0,2.3],
+                 [1.0,0.0,2.3],
+                 [1.0,1.0,0.3],
+                 [1.0,1.0,2.3],
+                 [3.4,4.5,4.3],
+                 [3.4,4.3,4.3],
+                 [3.4,5.5,4.3],
+                 [3.7,7.5,6.5]]
+        name = os.path.abspath(os.path.dirname(__file__))
+        name = os.path.join(name, "..", "..")
+        name = os.path.join(name, "TestUtilities", "Scratch")
+        trajectory_filename = os.path.join(name, "tmp_trajectory_file.py")
+        self.__files_to_remove.append(trajectory_filename)
+
+        # Construct.
+        t = Trajectory(trajectory_filename, sites)
+
+        # Write times, steps and typers.
+        times = [1.10045, 2.334156, 3.4516410]
+        steps = [12, 25, 52]
+        types = [["ThisIsTheLongestTypeNameIHaveEverEncounteredPerhaps",
+                  "here", "is", "Next", "Long", "List", "Offffffff", "Names", "now", "this", "one", "is", "longer", "still"],
+                 ["A", "B", "C", "D", "E", "F", "G", "H"],
+                 ["1", "2", "4", "5", "6", "5" ,"43", "243r2424"]]
+
+        # Check that the time is zero before we start.
+        self.assertAlmostEqual( t._Trajectory__time_last_dump, 0.0, 10 )
+
+        t._Trajectory__writeToFile(times, steps, types)
+
+        # Check that the time stamp was updated.
+        self.assertTrue( 1357651850 < t._Trajectory__time_last_dump )
+        last_time = t._Trajectory__time_last_dump
+
+        # Check the info stored in the file.
+        global_dict = {}
+        local_dict  = {}
+        execfile(trajectory_filename, global_dict, local_dict)
+
+        # Check the types.
+        ret_types = local_dict['types']
+        ref_types = [['ThisIsTheLongestTypeNameIHaveEverEncounteredPerhaps',
+                      'here', 'is', 'Next', 'Long', 'List', 'Offffffff',
+                      'Names', 'now', 'this', 'one', 'is', 'longer', 'still'],
+                     ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+                     ['1', '2', '4', '5', '6', '5', '43', '243r2424']]
+        self.assertEqual( ret_types, ref_types )
+
+        # Check the steps.
+        ret_steps = local_dict['steps']
+        ref_steps = [12, 25, 52]
+        self.assertEqual( ret_steps, ref_steps )
+
+        # Check the times.
+        ret_times = local_dict['times']
+        ref_times = [1.10045, 2.334156, 3.451641]
+        self.assertEqual( ret_times, ref_times )
+
+        # Sleep for two seconds before we add again.
+        time.sleep(1)
+        t._Trajectory__writeToFile(times, steps, types)
+
+        # Check the time.
+        self.assertTrue( (t._Trajectory__time_last_dump - last_time > 1) )
+
+        # Now, check the file again.
+        global_dict = {}
+        local_dict  = {}
+        execfile(trajectory_filename, global_dict, local_dict)
+
+        # Check the types.
+        ret_types = local_dict['types']
+        ref_types = [['ThisIsTheLongestTypeNameIHaveEverEncounteredPerhaps',
+                      'here', 'is', 'Next', 'Long', 'List', 'Offffffff',
+                      'Names', 'now', 'this', 'one', 'is', 'longer', 'still'],
+                     ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+                     ['1', '2', '4', '5', '6', '5', '43', '243r2424'],
+                     ['ThisIsTheLongestTypeNameIHaveEverEncounteredPerhaps',
+                      'here', 'is', 'Next', 'Long', 'List', 'Offffffff',
+                      'Names', 'now', 'this', 'one', 'is', 'longer', 'still'],
+                     ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+                     ['1', '2', '4', '5', '6', '5', '43', '243r2424']]
+        self.assertEqual( ret_types, ref_types )
+
+        # Check the steps.
+        ret_steps = local_dict['steps']
+        ref_steps = [12, 25, 52, 12, 25, 52]
+        self.assertEqual( ret_steps, ref_steps )
+
+        # Check the times.
+        ret_times = local_dict['times']
+        ref_times = [1.10045, 2.334156, 3.451641, 1.10045, 2.334156, 3.451641]
+        self.assertEqual( ret_times, ref_times )
+
+    def testAppend(self):
+        """ Test appending to the trajectory. """
+        # Setup input.
+        sites = [[0.0,1.0,2.3],
+                 [1.0,0.0,2.3],
+                 [1.0,1.0,0.3],
+                 [1.0,1.0,2.3],
+                 [3.4,4.5,4.3],
+                 [3.4,4.3,4.3],
+                 [3.4,5.5,4.3],
+                 [3.7,7.5,6.5]]
+        name = os.path.abspath(os.path.dirname(__file__))
+        name = os.path.join(name, "..", "..")
+        name = os.path.join(name, "TestUtilities", "Scratch")
+        trajectory_filename = os.path.join(name, "tmp_trajectory_file.py")
+        self.__files_to_remove.append(trajectory_filename)
+
+        # Construct.
+        t = Trajectory(trajectory_filename, sites)
+
+        # Append times, steps and typers.
+        t.append(1.10045, 12,
+                 ["A", "A", "A", "A", "A", "A"] )
+
+        # Since this was the first time it should have triggered a dump to file.
+        global_dict = {}
+        local_dict  = {}
+        execfile(trajectory_filename, global_dict, local_dict)
+
+        # Check the types.
+        ret_types = local_dict['types']
+        ref_types = [["A", "A", "A", "A", "A", "A"]]
+        self.assertEqual( ret_types, ref_types )
+
+        # Check the steps.
+        ret_steps = local_dict['steps']
+        ref_steps = [12]
+        self.assertEqual( ret_steps, ref_steps )
+
+        # Check the times.
+        ret_times = local_dict['times']
+        ref_times = [1.10045]
+        self.assertEqual( ret_times, ref_times )
+
+        # Appending again directly makes no dump.
+        t.append(1.1993, 14,
+                 ["B", "B", "B", "B", "B", "B"] )
+
+        global_dict = {}
+        local_dict  = {}
+        execfile(trajectory_filename, global_dict, local_dict)
+
+        # Check.
+        ret_types = local_dict['types']
+        self.assertEqual( ret_types, ref_types )
+        ret_steps = local_dict['steps']
+        self.assertEqual( ret_steps, ref_steps )
+        ret_times = local_dict['times']
+        self.assertEqual( ret_times, ref_times )
+
+        # But if we dump again and set the time limit to zero we will trigger a dump.
+        t._Trajectory__max_buffer_time = 0.0
+        t.append(1.199, 19,
+                 ["C", "C", "C", "C", "C", "C"] )
+
+        # Reset the time to some thing large.
+        t._Trajectory__max_buffer_time = 100000000000.0
+
+        global_dict = {}
+        local_dict  = {}
+        execfile(trajectory_filename, global_dict, local_dict)
+
+        # Check the types.
+        ret_types = local_dict['types']
+        ref_types = [["A","A","A","A","A","A"],
+                     ["B","B","B","B","B","B"],
+                     ["C","C","C","C","C","C"]]
+        self.assertEqual( ret_types, ref_types )
+
+        # Check the steps.
+        ret_steps = local_dict['steps']
+        ref_steps = [12,14,19]
+        self.assertEqual( ret_steps, ref_steps )
+
+        # Check the times.
+        ret_times = local_dict['times']
+        ref_times = [1.10045, 1.1993, 1.199]
+        self.assertEqual( ret_times, ref_times )
+
+        # The buffers are reset after each dump. If we make the
+        # max size limit smaller than the size of the appended types
+        # list this must trigger a dump.
+        types = ["A","A","A","A","A","B"]
+        size = sys.getsizeof(types)
+        t._Trajectory__max_buffer_size = size
+
+        # Append.
+        t.append(1.1995, 43, types )
+
+        # Check.
+        global_dict = {}
+        local_dict  = {}
+        execfile(trajectory_filename, global_dict, local_dict)
+
+        ret_types = local_dict['types']
+        ref_types = [["A","A","A","A","A","A"],
+                     ["B","B","B","B","B","B"],
+                     ["C","C","C","C","C","C"]]
+        self.assertEqual( ret_types, ref_types )
+
+        # Append.
+        t.append(1.1995, 43, types )
+        t.append(1.1995, 43, types )
+        t.append(1.1995, 43, types )
+        # This last one triggers the dump.
+        t.append(1.1995, 43, types )
+
+        # Check.
+        global_dict = {}
+        local_dict  = {}
+        execfile(trajectory_filename, global_dict, local_dict)
+
+        ret_types = local_dict['types']
+        ref_types = [["A","A","A","A","A","A"],
+                     ["B","B","B","B","B","B"],
+                     ["C","C","C","C","C","C"],
+                     ["A","A","A","A","A","B"],
+                     ["A","A","A","A","A","B"],
+                     ["A","A","A","A","A","B"],
+                     ["A","A","A","A","A","B"],
+                     ["A","A","A","A","A","B"]]
+        self.assertEqual( ret_types, ref_types )
+
 
 if __name__ == '__main__':
     unittest.main()
