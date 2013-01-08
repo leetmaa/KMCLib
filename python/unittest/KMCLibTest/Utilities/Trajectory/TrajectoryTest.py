0726dc6e5ce1d089d18f70edbf2c21a7b4e5c6b0
Mikael Leetmaa <mikael.leetmaa@gmail.com>
2013-01-08 15:34:06 +0100

    Misc #00000: Updating copyright info.

--- python/unittest/KMCLibTest/Utilities/Trajectory/TrajectoryTest.py
+++ python/unittest/KMCLibTest/Utilities/Trajectory/TrajectoryTest.py
@@ -1,7 +1,7 @@
 """ Module for testing the Trajectory object. """
 
 
-# Copyright (c)  2012  Mikael Leetmaa
+# Copyright (c)  2013  Mikael Leetmaa
 #
 # This file is part of the KMCLib project distributed under the terms of the
 # GNU General Public License version 3, see <http://www.gnu.org/licenses/>.
--- python/unittest/KMCLibTest/Utilities/Trajectory/TrajectoryTests.py
+++ python/unittest/KMCLibTest/Utilities/Trajectory/TrajectoryTests.py
@@ -1,6 +1,6 @@
 
 
-# Copyright (c)  2012  Mikael Leetmaa
+# Copyright (c)  2013  Mikael Leetmaa
 #
 # This file is part of the KMCLib project distributed under the terms of the
 # GNU General Public License version 3, see <http://www.gnu.org/licenses/>.
