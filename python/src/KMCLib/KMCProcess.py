e04ab03811cd88b7eeef91ad8452651622394a00
Mikael Leetmaa <mikael.leetmaa@gmail.com>
2013-03-04 11:53:01 +0100

    Task #0046: Constructor documentation.

--- python/src/KMCLib/KMCProcess.py
+++ python/src/KMCLib/KMCProcess.py
@@ -10,12 +10,49 @@
 
 class KMCProcess(object):
     """
-    Class for holding information about a process in a lattice KMC simulation.
+    Class for representing a possible process in a lattice KMC simulation.
     """
 
-    def __init__(self):
+    def __init__(self,
+                 coordinates=None,
+                 elements_before=None,
+                 elements_after=None,
+                 basis_sites=None,
+                 implicit_wildcards=None):
         """
         Constructor for the KMCProcess.
+
+        :param coordinates: The local coordinates, corresponding to the lattice
+                            positions of the surrounding of the center where
+                            the process will be preformed.
+
+        :param elements_before: The elements, as a list of strings,
+                                before the process is preformed.
+                                This list of elements will be used to match the
+                                local surroundings of each center in the
+                                simulation, to determine at which sites the
+                                process can be performed.
+
+        :param elements_after: The elements, as a list of strings,
+                               after the process is preformed.
+                               This list of elements will be used to update the
+                               local surrounding of the site where the process
+                               is performed.
+
+        :param basis_sites: The basis sites in the lattice at which the process
+                            can possibly be applied. Only if the length of this
+                            list is 1 can implicit wildcards be used for the
+                            matching.
+
+        :param implicit_wildcards: A flag to indicate if implicit wildcards
+                                   should be used in the matching when there
+                                   are more sites in the local geometry on the
+                                   lattice than given in the coordinates and
+                                   element lists to specify the process. This
+                                   keyord only takes effect if the basis_sites
+                                   list has the length 1, i.e. that the process
+                                   can only be applied to on specific basis
+                                   site in the primitive cell.
         """
         # NEEDS IMPLEMENTATION
         pass
