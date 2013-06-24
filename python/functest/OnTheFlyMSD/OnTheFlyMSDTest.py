833bf1aa7a393b5a3b4f6075174afc83c086bd9b
Mikael Leetmaa <mikael.leetmaa@gmail.com>
2013-06-24 14:03:38 +0200

    Task #0063: Finalizing C++ MSD version and testing from python.

--- python/functest/OnTheFlyMSD/OnTheFlyMSDTest.py
+++ python/functest/OnTheFlyMSD/OnTheFlyMSDTest.py
@@ -13,8 +13,6 @@ import numpy
 # Import the interface.
 from KMCLib import *
 
-from KMCLib.OnTheFlyMSD.FastOnTheFlyMSD import FastOnTheFlyMSD
-
 # Implement the test.
 class OnTheFlyMSDTest(unittest.TestCase):
     """ Class for testing 1D diffusion. """
@@ -76,11 +74,10 @@ class OnTheFlyMSDTest(unittest.TestCase):
                                                   analysis_interval=10)
         # Run the model.
         model.run(control_parameters=control_parameters,
-                  trajectory_filename="traj.py",
                   analysis=[msd])
 
-        with open('msd.data', 'w') as f:
-            msd.result(f)
+        with open('msd.data1', 'w') as f:
+            msd.printResult(f)
 
         # NEEDS IMPLEMENTATION
 
@@ -144,71 +141,7 @@ class OnTheFlyMSDTest(unittest.TestCase):
                   analysis=[msd])
 
         with open('msd.data2', 'w') as f:
-            msd.result(f)
-
-        # NEEDS IMPLEMENTATION
-
-    def test1DLongRandomWalkFast(self):
-        """ Run the model and check the results. """
-        # Setup a system, a periodic 1000 atoms long 1D chain.
-        unit_cell = KMCUnitCell(cell_vectors=numpy.array([[1.0,0.0,0.0],
-                                                          [0.0,1.0,0.0],
-                                                          [0.0,0.0,1.0]]),
-                                basis_points=[[0.0,0.0,0.0]])
-
-        # And a lattice.
-        lattice = KMCLattice(unit_cell=unit_cell,
-                             repetitions=(1000,1,1),
-                             periodic=(True,False,False))
-
-        # Setup an initial configuration with one B in a sea of A:s.
-        types = ["A"]*1000
-        types[5] = "B"
-
-        config = KMCConfiguration(lattice=lattice,
-                                  types=types,
-                                  possible_types=["A","B"])
-
-        # Setup a diffusion process to the left.
-        coordinates_p0 = [[0.0, 0.0, 0.0],[-1.0, 0.0, 0.0]]
-        p0 = KMCProcess(coordinates=coordinates_p0,
-                        elements_before=["B","A"],
-                        elements_after=["A","B"],
-                        move_vectors=None,
-                        basis_sites=[0],
-                        rate_constant=1.0)
-
-        coordinates_p1 = [[0.0, 0.0, 0.0],[1.0, 0.0, 0.0]]
-        p1 = KMCProcess(coordinates=coordinates_p1,
-                        elements_before=["B","A"],
-                        elements_after=["A","B"],
-                        move_vectors=None,
-                        basis_sites=[0],
-                        rate_constant=1.0)
-
-        interactions = KMCInteractions(processes=[p0, p1],
-                                       implicit_wildcards=True)
-
-        model = KMCLatticeModel(configuration=config,
-                                interactions=interactions)
-
-        # Setup the analysis.
-        msd = FastOnTheFlyMSD(history_steps=40,
-                              n_bins=1000,
-                              t_max=2500.0,
-                              track_type="B")
-
-        # Setup the control parameters.
-        control_parameters = KMCControlParameters(number_of_steps=4000,
-                                                  dump_interval=1000,
-                                                  analysis_interval=1)
-        # Run the model.
-        model.run(control_parameters=control_parameters,
-                  trajectory_filename="traj.py",
-                  analysis=[msd])
-
-        with open('msd.data3', 'w') as f:
-            msd.result(f)
+            msd.printResult(f)
 
         # NEEDS IMPLEMENTATION
 
