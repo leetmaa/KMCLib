834a6b380344ec06c900bc3745bdf6649042594d
Mikael Leetmaa <mikael.leetmaa@gmail.com>
2014-06-05 12:49:12 +0200

    Task #0110: Match list operators on the class.

--- c++/src/matchlistentry.h
+++ c++/src/matchlistentry.h
@@ -16,6 +16,13 @@
 #include "coordinate.h"
 #include <cmath>
 
+
+/*! \brief The epsilon value for comparing lattice positions.
+ */
+static double epsi__ = 1.0e-5;
+
+
+/// The base class for the match list entries.
 class MatchListEntry {
 
 public:
@@ -53,6 +60,61 @@ public:
     /// The move coordinate.
     Coordinate move_coordinate;
 
+    /// The match types used in bucket mode.
+    std::vector<int> match_types;
+
+    /// The minimum number of particles of matching type for bucket match.
+    int match_min_quantity;
+
+    /*! \brief 'equal' for comparing points. NOTE: This operator does not
+     *         compare match types.
+     */
+    bool operator==(const MatchListEntry & m2) const
+    {
+        // Check the distance.
+        if (std::fabs(m2.distance - distance) > epsi__)
+        {
+            return false;
+        }
+        // Check the coordinate.
+        else if (std::fabs(m2.coordinate.x() - coordinate.x()) > epsi__)
+        {
+            return false;
+        }
+        else if (std::fabs(m2.coordinate.y() - coordinate.y()) > epsi__)
+        {
+            return false;
+        }
+        else if (std::fabs(m2.coordinate.z() - coordinate.z()) > epsi__)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /*! \brief 'less than' for sorting matchlists.
+     */
+    bool operator<(const MatchListEntry & m2) const
+    {
+        // Sort along distance, type and coordinate.
+        if (std::fabs(m2.distance - distance) < epsi__)
+        {
+            // If the distances are practically the same,
+            // check the coordinate.
+            return (coordinate < m2.coordinate);
+        }
+        else
+        {
+            // Sort wrt. distance.
+            return (distance < m2.distance);
+        }
+    }
+
+    /*! \brief Pure virtual 'not equal' operator.
+     */
+    virtual
+    bool operator!=(const MatchListEntry & m2) const=0;
+
 
 protected:
 
@@ -62,138 +124,74 @@ protected:
 };
 
 
-/// Match list entry for the process bucket match list.
-class ProcessBucketMatchListEntry : public MatchListEntry {
-
-public:
-    // ML: Fill with values.
-
-    /// The minimum number of particles of matching type for match.
-    int match_min_quantity;
-
-
-};
-
-
 /// Match list entry for the configuration bucket match list.
 class ConfigBucketMatchListEntry : public MatchListEntry {
 
 public:
-    /// The match types.
-    std::vector<int> match_types;
 
+    virtual
+    bool operator!=(const MatchListEntry & m2) const
+    { // Throw an error.
+        exit(0);
+    }
 };
 
 
-/// A minimal struct to replace the MatchListEntry class.
+/// Match list entry for the process bucket match list.
+class ProcessBucketMatchListEntry : public MatchListEntry {
+
+public:
+
+    /*! \brief 'not equal' for comparing entries.
+     */
+    virtual
+    bool operator!=(const MatchListEntry & m2) const
+    {
+        // Handle the wildcard case.
+        if (match_type == -1)
+        {
+            return false;
+        }
+
+        // Check the quantity at the specific match type.
+        else if (m2.match_types[match_type] < match_min_quantity)
+        {
+            return true;
+        }
+        else
+        {
+            return !((*this) == m2);
+        }
+    }
+};
+
+
+/// A minimal match list entry class.
 class MinimalMatchListEntry : public MatchListEntry {
 
 public:
 
+    /*! \brief 'not equal' for comparing entries.
+     */
+    virtual
+    bool operator!=(const MatchListEntry & m2) const
+    {
+        // Handle the wildcard case.
+        if (match_type == 0)
+        {
+            return false;
+        }
+        // Check the type.
+        else if (m2.match_type != match_type)
+        {
+            return true;
+        }
+        else
+        {
+            return !((*this) == m2);
+        }
+    }
 };
-
-
-/*! \brief The epsilon value for comparing lattice positions.
- */
-static double epsi__ = 1.0e-5;
-
-
-/*! \brief 'equal' for comparing points. NOTE: This operator does not
- *         compare match types.
- */
-inline
-bool operator==(const MatchListEntry & m1,
-                const MatchListEntry & m2)
-{
-    // Check the distance.
-    if (std::fabs(m2.distance - m1.distance) > epsi__)
-    {
-        return false;
-    }
-    // Check the coordinate.
-    else if (std::fabs(m2.coordinate.x() - m1.coordinate.x()) > epsi__)
-    {
-        return false;
-    }
-    else if (std::fabs(m2.coordinate.y() - m1.coordinate.y()) > epsi__)
-    {
-        return false;
-    }
-    else if (std::fabs(m2.coordinate.z() - m1.coordinate.z()) > epsi__)
-    {
-        return false;
-    }
-    return true;
-}
-
-
-/*! \brief 'not equal' for comparing entries.
- */
-inline
-bool operator!=(const MinimalMatchListEntry & m1,
-                const MinimalMatchListEntry & m2)
-{
-    // Handle the wildcard case.
-    if (m1.match_type == 0)
-    {
-        return false;
-    }
-    // Check the type.
-    else if (m2.match_type != m1.match_type)
-    {
-        return true;
-    }
-    else
-    {
-        return !(m1 == m2);
-    }
-}
-
-
-/*! \brief 'less than' for sorting matchlists.
- */
-inline
-bool operator<(const MatchListEntry & m1,
-               const MatchListEntry & m2)
-{
-
-    // Sort along distance, type and coordinate.
-    if (std::fabs(m2.distance - m1.distance) < epsi__)
-    {
-        // If the distances are practically the same,
-        // check the coordinate.
-        return (m1.coordinate < m2.coordinate);
-    }
-    else
-    {
-        // Sort wrt. distance.
-        return (m1.distance < m2.distance);
-    }
-}
-
-
-/*! \brief 'not equal' for comparing entries.
- */
-inline
-bool operator!=(const ProcessBucketMatchListEntry & m1,
-                const ConfigBucketMatchListEntry & m2)
-{
-    // Handle the wildcard case.
-    if (m1.match_type == -1)
-    {
-        return false;
-    }
-
-    // Check the quantity at the specific match type.
-    else if (m2.match_types[m1.match_type] < m1.match_min_quantity)
-    {
-        return true;
-    }
-    else
-    {
-        return !(m1 == m2);
-    }
-}
 
 
 #endif // __MATCHLISTENTRY__
