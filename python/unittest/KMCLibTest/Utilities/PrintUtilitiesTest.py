44839b20bac6a2f8d6d9a4d79f1ee5c4ea249326
Mikael Leetmaa <mikael.leetmaa@gmail.com>
2016-03-29 00:00:16 +0200

    Misc #0000: Final bump to v2.0 alpha.

--- python/unittest/KMCLibTest/Utilities/PrintUtilitiesTest.py
+++ python/unittest/KMCLibTest/Utilities/PrintUtilitiesTest.py
@@ -73,7 +73,7 @@ class PrintUtilitiesTest(unittest.TestCase):
             # Check.
             if MPICommons.myRank() == 0:
                 ref_str = """# -----------------------------------------------------------------------------
-# KMCLib version 2.0.a1
+# KMCLib version 2.0-a1
 # Distributed under the GPLv3 license
 # Copyright (C)  2012-2016  Mikael Leetmaa
 # Developed by Mikael Leetmaa <leetmaa@kth.se>
